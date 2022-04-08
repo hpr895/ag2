767fe4644868c16570e8c4b61efb15b5d7f2863c
dsavinkov <hpr895@mail.ru>
2022-04-08 11:32:17 +0600

    all sticky ready

--- js/_demo.js
+++ js/_demo.js
@@ -1,21 +1,9 @@
-var Sticky = function (selector, padding, config) {
+var StickyHeader = function (selector) {
     var $sticky = document.querySelector(selector);
-    var $padding = document.querySelector(padding);
-
-    var paddingOffset;
-    var elHeight;
-
-    config = config || {}
-    var options = {
-        dynamic: config.dynamic || true,
-        offset: config.offset || 0,
-        translate: config.translate || false,
-        track: config.track ? document.querySelector('config.track') : false,
-    };
+    var $padding = $sticky.parentNode;
 
     if ($sticky && $padding) {
         setEvents();
-        setValues();
         refresh();
     }
 
@@ -25,50 +13,25 @@ var Sticky = function (selector, padding, config) {
     }
 
     function refresh() {
-        if (options.dynamic) {
-            setValues();
-        }
+        var paddingOffset = $padding.getBoundingClientRect().top;
+        var elHeight = $sticky.clientHeight;
 
-            // currentTrackOffset = config.track.clientHeight
-        var negativeOffset
-
-        if (paddingOffset < options.offset) {
+        if (paddingOffset < 0) {
             $sticky.classList.add('is-stuck');
-
-            if (options.translate) {
-                $sticky.style.transform = 'translateY(' + (window.scrollY - $sticky.clientHeight) + 'px)';
-            } else {
-                $padding.style.paddingBottom = elHeight + 'px';
-            }
+            $padding.style.paddingBottom = elHeight + 'px';
         } else {
             $sticky.classList.remove('is-stuck');
             $padding.style.paddingBottom = 0;
-            $sticky.style.transform = 'translateY(0)';
         }
     }
-
-    function setValues() {
-        paddingOffset = getTopOffset($padding);
-        elHeight = $sticky.clientHeight;
-    }
-
-    function getTopOffset($el) {
-        return $el.getBoundingClientRect().top;
-    }
-
-    // function calcoffsetFromTrack() {
-
-    // }
 };
 
 document.addEventListener('DOMContentLoaded', function () {
     (function () {
-        var stickyHeader = new Sticky('.js-sticky-header', '.js-sticky-header-padding');
+        var stickyHeader = new StickyHeader('.js-sticky-header');
 
-        var stickySidebar = new Sticky('.js-sticky-sidebar', '.js-sticky-sidebar-padding', {
-            offset: document.querySelector('.js-sticky-header').clientHeight,
-            translate: true,
-            track: 'js-sticky-sidebar-track',
+        var stickyEl = new Sticksy('.js-sticky-sidebar', {
+            topSpacing: document.querySelector('.js-sticky-header').clientHeight,
         });
     })();
 });
